b9cca3bfee3fa50f30a0aedd6fad5f8391cb4795
anmolk18 <anmolpreet.kaur@yale.edu>
2020-07-09 14:01:57 -0400

    done

--- allFiles/App/db/migrate/20200701175504_add_image_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddImageToUsers < ActiveRecord::Migration[6.0]
-    def change
-        add_column :users, :image, :string, :default => "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
-     end
-end
